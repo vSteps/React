2f9b3b4ff7a7bdf4109fe06d8a1c9aa0b9f358ab
vSteps <victor.passos@escolar.ifrn.edu.br>
2024-11-02 11:58:29 -0300

    :sparkles: feat: Utilização do props-types

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,15 @@
-function Item(props){
+import PropTypes from 'prop-types'
+function Item({marca = 'A marca está faltando, jegue', ano_lancamento = 0}){
     return(
         <>
-        <li>{props.marca}</li>
+        <li>{marca} - {ano_lancamento}</li>
         </>
     )
+}
+
+Item.propTypes = {
+    marca: PropTypes.string.isRequired,
+    ano_lancamento: PropTypes.number.isRequired,
 }
 
 export default Item
--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,15 @@
+
 import Item from './Item'
 function List(){
     return(
         <>
         <h1>Minha Lista</h1>
         <ul>
-            <Item marca = "Ferrari" />
-            <Item marca = "Fiat" />
+            <Item marca = "Ferrari" ano_lancamento = {1985} />
+            <Item marca = "Fiat" ano_lancamento = {1964} />
+            <Item marca = "Renault"/>
+            <Item marca = "Chevrolet" ano_lancamento ={1999}/>
+            <Item/>
         </ul>
         </>
     )
